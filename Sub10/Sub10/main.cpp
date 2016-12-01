24b7d0328f3583647088b42a9a1390bf8c214756
migak617 <wodnr2114@naver.com>
2016-12-01 10:08:51 +0900

    this is real practice

--- Sub10/Sub10/main.cpp
+++ Sub10/Sub10/main.cpp
@@ -5,9 +5,33 @@
 #include "frame.h"
 #include "exArray.h"
 #include "swap.h"
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
 
 using namespace std;
 
+class Word {
+private:
+	string word;
+public:
+	Word(string word) {
+		this->word = word;
+	}
+	string getWord() {
+		return this->word;
+	}
+};
+
+void dump(list<string> &l) {
+	list<string>::iterator iter = l.begin();
+
+	while (iter != l.end()) {
+		cout << *iter << endl;
+		iter++;
+	}
+}
 
 Game* game = new Game();
 
@@ -55,7 +79,6 @@ int main() {
 	{
 		cout << "Error msg : " << msg << endl;
 	}
-	*/
 	Exarray<int> arr1(3);
 	Exarray<char> arr2(4);
 	Exarray<double> arr3(5);
@@ -84,5 +107,83 @@ int main() {
 	mySwap<Exarray<int>>(arr1, arr4);
 	arr1.printData();
 	arr4.printData();
+	*/
+	/*	12.01 ½Ç½À
+	vector <int> v;
+
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.insert(v.begin(), 40);
+	v.insert(v.end(), 50);
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}
+	cout << "access index at 1 : " << v.at(1) << endl;
+
+	v.erase(v.begin());
+	v.erase(v.end() - 1);
+
+	vector<int> ::iterator iter = v.begin();
+
+	cout << "access index at 1 with iterator : " << iter[1] << endl;
+
+	while (iter != v.end()) {
+		cout << *iter << endl;
+		iter++;
+	}
+	
+	list<string> names;
+	names.insert(names.begin(), "Konkuk");
+	names.insert(names.end(), "University");
+	names.insert(names.end(), "SCLAB");
+	names.insert(names.end(), "CSY");
+	dump(names);
+	cout << "====" << endl;
+	names.reverse();
+	dump(names);
+	
+	map<string, int> m;
+	m["seoul"] = 100;
+	m["daegu"] = 120;
+	m["busan"] = 200;
+
+	cout << "Train to Seoul = " << m["seoul"] << "$" << endl;
+	cout << "Train to Seoul = " << m["daegu"] << "$" << endl;
+	cout << "Train to Seoul = " << m["busan"] << "$" << endl;
+	*/
+	map<char, list<Word>> m;
+
+	list<Word> aList;
+	list<Word> bList;
+
+	aList.push_back(Word("apple"));
+	aList.push_back(Word("avoid"));
+	aList.push_back(Word("appear"));
+
+	bList.push_back(Word("bread"));
+	bList.push_back(Word("bring"));
+	bList.push_back(Word("bow"));
+
+	m['a'] = aList;
+	m['b'] = bList;
+
+	map<char, list<Word>>::const_iterator iter = m.begin();
+
+	while (iter != m.end()) {
+		cout << iter->first << ":" << endl;
+
+		list<Word> temp = iter->second;
+		list<Word>::const_iterator iter_list = temp.begin();
+
+		while (iter_list != temp.end()) {
+			Word word = *iter_list;
+			cout << word.getWord() << ", ";
+			iter_list++;
+		}
+		cout << "\n===" << endl;
+		iter++;
+	}
 	return 0;
 }
